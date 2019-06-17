972114812f048c8377d41657be675077d187d3ee
Fedor Yakubovich <f.yakubovich@gmail.com>
2019-06-17 15:28:53 +0300

    Deleted chai

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { expect } from 'chai';
 import { shallow, mount } from 'enzyme';
 
 import ReactSalute from '../lib';
@@ -8,14 +7,14 @@ describe('<ReactSalute />', () => {
   it('Check default message', () => {
     const wrapper = shallow(<ReactSalute />);
 
-    expect(wrapper.text()).to.equal('Salute React!');
+    expect(wrapper.text()).toBe('Salute React!');
   });
 
   it('Check message passed through props', () => {
     const name = 'Foo';
     const wrapper = shallow(<ReactSalute name={name} />);
 
-    expect(wrapper.text()).to.equal(`Salute ${name}!`);
+    expect(wrapper.text()).toBe(`Salute ${name}!`);
   });
 
   it('Check reset props', () => {
@@ -23,8 +22,8 @@ describe('<ReactSalute />', () => {
     const newName = 'Bar';
     const wrapper = mount(<ReactSalute name={name} />);
 
-    expect(wrapper.props().name).to.equal(name);
+    expect(wrapper.props().name).toBe(name);
     wrapper.setProps({ name: newName });
-    expect(wrapper.props().name).to.equal(newName);
+    expect(wrapper.props().name).toBe(newName);
   });
 });
